db3a750920d24dcac3991615ca0c1b4712dde70c
kopalych <kopalychmail@gmail.com>
2026-03-27 16:09:32 +0100

    Task 2

---
+++ js/task-2.js
@@ -0,0 +1,18 @@
+function getShippingMessage(country, price, deliveryFee) {
+  if (
+    !(typeof country === 'string') ||
+    !Number.isInteger(price) ||
+    !Number.isInteger(deliveryFee)
+  )
+    throw new TypeError(
+      `Expected str and two ints, got: ${typeof country}, ${typeof price}, ${typeof deliveryFee}`
+    );
+
+  return `Shipping to ${country} will cost ${price + deliveryFee} credits`;
+}
+
+console.log('----- TASK 2 -----');
+console.log(getShippingMessage('Australia', 120, 50)); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingMessage('Germany', 80, 20)); // "Shipping to Germany will cost 100 credits"
+console.log(getShippingMessage('Sweden', 100, 20)); // "Shipping to Sweden will cost 120 credits"
+console.log('');
