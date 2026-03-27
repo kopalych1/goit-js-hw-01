6e4aaadce46690f888dc3d4475facbbad6e612e3
kopalych <kopalychmail@gmail.com>
2026-03-27 16:41:35 +0100

    Task 3

---
+++ js/task-3.js
@@ -0,0 +1,27 @@
+function getElementWidth(content, padding, border) {
+  if (
+    typeof content !== 'string' ||
+    typeof padding !== 'string' ||
+    typeof border !== 'string'
+  )
+    throw new TypeError(
+      `Expected three strings, got: ${typeof content}, ${typeof padding}, ${typeof border}`
+    );
+  if (
+    !content.trim().endsWith('px') ||
+    !padding.trim().endsWith('px') ||
+    !border.trim().endsWith('px')
+  )
+    throw new TypeError(
+      `Numbers must be in pixels: 'Npx', got: '${content}' '${padding}' '${border}'`
+    );
+
+  return Math.round(
+    parseFloat(content) + parseFloat(padding) * 2 + parseFloat(border) * 2
+  );
+}
+
+console.log('----- TASK 3 -----');
+console.log(getElementWidth('50px', '8px', '4px')); // 74
+console.log(getElementWidth('60px', '12px', '8.5px')); // 101
+console.log(getElementWidth('200px', '0px', '0px')); // 200
