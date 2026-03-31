bb257127e203bf40b974b41a648a3d94cf10fea4
kopalych <kopalychmail@gmail.com>
2026-03-31 18:48:19 +0200

    Task 3: fix: getting rid of unnecessary round

--- js/task-3.js
+++ js/task-3.js
@@ -16,12 +16,11 @@ function getElementWidth(content, padding, border) {
       `Numbers must be in pixels: 'Npx', got: '${content}' '${padding}' '${border}'`
     );
 
-  return Math.round(
-    parseFloat(content) + parseFloat(padding) * 2 + parseFloat(border) * 2
-  );
+  return parseFloat(content) + parseFloat(padding) * 2 + parseFloat(border) * 2;
 }
 
 console.log('----- TASK 3 -----');
 console.log(getElementWidth('50px', '8px', '4px')); // 74
 console.log(getElementWidth('60px', '12px', '8.5px')); // 101
 console.log(getElementWidth('200px', '0px', '0px')); // 200
+console.log(getElementWidth('200px', '0.25px', '1.3px')); // 203.1
